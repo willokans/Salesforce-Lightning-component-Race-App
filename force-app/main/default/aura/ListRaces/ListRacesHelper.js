3ab5d954bd57e65f2f7c28ef5fb45270b2c07377
Will <willokans@outlook.com>
2020-02-23 15:20:57 +0000

    add apex controller class

--- force-app/main/default/aura/ListRaces/ListRacesHelper.js
+++ force-app/main/default/aura/ListRaces/ListRacesHelper.js
@@ -1,5 +1,21 @@
 ({
-    helperMethod : function() {
-
+    getRaces : function(component) {
+        // What needs to happen when calling an apex controller action
+        //1. Get the action
+        var action = component.get('c.getRacesDB');
+        
+        //2. Set its parameters
+        action.setParams({"race" : newRace});
+        
+        //3. Define the callback
+        action.setCallback(this, function(response){
+            var state = response.getState();
+            if (component.isValid() && state == "SUCCESS") {
+                component.set("v.races", response.getReturnValue());
+            }
+        })
+        
+        //4. Enqueue the aciton
+        $A.enqueueAction(action);
     }
 })
--- force-app/main/default/aura/RaceTrackerApp/RaceTrackerApp.app
+++ force-app/main/default/aura/RaceTrackerApp/RaceTrackerApp.app
@@ -1,4 +1,16 @@
 <aura:application >
-    <c.NewRace />
-    <c.ListRaces />
+
+<div class="slds">
+    <div class="slds-page-header">
+        <p class="slds-page-header__title" title="Race Tracker">Race Tracker</p>
+    </div>
+
+    <div class="slds-m-around--small slds-p-top--large">
+            <c.NewRace />
+    </div>
+
+    <div class="slds-m-around--small slds-p-top--large">
+            <c.ListRaces />
+    </div>
+</div>
 </aura:application>
